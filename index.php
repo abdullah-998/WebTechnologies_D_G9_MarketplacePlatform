30aa98cfae8ccd1c732b0ac9d3d24107a5c2f4a9
unknown <abdullah.jayedoo@gmail.com>
2020-11-17 20:05:18 +0600

    index done

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1" align="center">
+        <tr>
+            <td height="60px" width="900px">
+                <img src="asset/logo.png" height="60" width="110">
+                <a href="view/log.php">log in</a>|
+                <a href="view/reg.php">Registration</a>|
+                <a href="index.php">Mareket Place</a>
+            </td>
+        </tr>
+        <tr>
+            <td height="700px" width="900px">
+                <h1>Wel Come to world largest freelancer site</h1>
+                <p>where you can find out variety of freelancer all over the world</p><br>
+                Don't have account?
+                <input type="button" value="Sign up">
+            </td>
+        </tr>
+        <tr>
+            <td height="60px" width="900px" align="center">
+                copyright &copy 2020
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
