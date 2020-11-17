7d08ceebae1330166a33c3d77561fba920949bc5
unknown <abdullah.jayedoo@gmail.com>
2020-11-17 21:44:48 +0600

    message done

---
+++ view/message.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Message</title>
+</head>
+<body>
+    <table border="1" align="center">
+        <tr>
+            <td height="40px" width="300px"><img src="../asset/msg.png" height="35px" width="40px"><b>Messages</b></td>
+            <td height="40px" width="230px">
+                <img src="../asset/user.png" height="35px" width="35px"><b>Name of User</b>
+            </td>
+        </tr>
+        <tr>
+            <td height="500px" width="280px" rowspan="2"><hr>
+                <img src="../asset/user.png" height="35px" width="35px">Name of User 1<br><hr>
+                <img src="../asset/user.png" height="35px" width="35px">Name of User 2<br><hr>
+                <img src="../asset/user.png" height="35px" width="35px">Name of User 3<br><hr>
+            </td>
+            <td height="500px" width="250px">
+                hello
+            </td>
+        </tr>
+        <tr>
+            <td height="34px" width="280px">
+                <input type="text" name="msgbox">
+                <input type="submit" value="send" name="submit">
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
