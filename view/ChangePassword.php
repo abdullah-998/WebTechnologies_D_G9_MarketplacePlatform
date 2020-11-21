50dd2e087d41842751c082f4dfb3e2d35ef76de6
unknown <abdullah.jayedoo@gmail.com>
2020-11-21 10:03:57 +0600

    Change password done

---
+++ view/ChangePassword.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Change Password</title>
+</head>
+<body>
+    <table border="1" align="center">
+        <tr>
+            <td height="60px" width="900px" colspan="2">
+                <img src="../asset/logo.png" height="60" width="110">
+                <a href="../index.php">Mareket Place</a>
+                <input type="text" placeholder="search..." name="searchbox">
+                <input type="button" value="Search" name="btnserach">
+                <a href="message.php">Message</a>
+                <a href="notification.php">Notification</a>
+                <a href="../php/logout.php">Log out</a>
+                <a href="home.php"><img src="../asset/user.png" height="40px" width="40px"></a>
+            </td>
+        </tr>
+        <tr>
+            <td height="500px" width="200px">
+                <img src="../asset/user.png" height="180px" width="180px"><br>
+                <?php echo "<b>User Name</b>";?><hr>
+                <ul>
+                <li><a href="home.php">Home</a></li>
+                    <li><a href="editProfile.php">Edit Profile</a></li>
+                    <li><a href="ChangePicture.php">Change Profile Picture</a></li>
+                    <li><a href="ChangePassword.php">Change Password</a></li>
+                    <li><a href="UpdateGeeg.php">Change Geeg</a></li>
+                    <li><a href="RunningProject.php">Running Project</a></li>
+                    <li><a href="Deactivate.php">Deactivate</a></li>
+                </ul>
+            </td>
+            <td height="500px" width="700px" align="center">
+                <form action="../php/cPassCheck.php" method="POST">
+                    <fieldset>
+                        <legend><b>Change Password</b></legend>
+                        Old password:<input type="password" name="oldpass"> <br><hr>
+                        New password:<input type="password" name="newpass"><br><hr>
+                        Confirm password:<input type="password" name="cnewpass"><br><hr>
+                        Do you sure want to change password?
+                        <input type="submit" name="submit" value="change password">
+                    </fieldset>
+                </form>
+            </td>
+        </tr>
+        <tr>
+            <td height="60px" width="900px" colspan="2" align="center">
+            copyright &copy 2020
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
